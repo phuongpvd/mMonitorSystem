dbe29b646355d0d884c81a414c0ed20d48908a0f
phuong <phuong@PHUONG-T61-COM>
2013-09-17 15:14:16 +0700

    [GIT] Commit phonegap_installer.sh

---
+++ phonegap_installer.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# download nodejs tarball
+wget http://nodejs.org/dist/v0.10.18/node-v0.10.18.tar.gz
+# untar nodejs tarball
+tar xvf node-v0.10.18.tar.gz
+# go inside folder
+cd node-v0.10.18
+
+# do config nodejs
+./configure
+# build nodejs
+make
+# install nodejs
+sudo make install
+
+# install phonegap
+sudo npm install -g phonegap
+
+# install cordova
+sudo npm install -g cordova
